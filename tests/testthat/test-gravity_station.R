08c81b84b590d697d9878777804539a0a179874a
jkennel <jkennel@uoguelph.ca>
2019-03-02 19:47:42 -0500

    remove export

--- tests/testthat/test-gravity_station.R
+++ tests/testthat/test-gravity_station.R
@@ -2,11 +2,11 @@ context("test-gravity_station")
 
 test_that("gravity_station works", {
 
-  expect_equal(earthtide::gravity_station(0,0), 9.78032677, tolerance = 1e-6)
-  expect_equal(earthtide::gravity_station(pi/2, 0), 9.832186, tolerance = 1e-6)
-  expect_equal(earthtide::gravity_station(pi/4, 0), 9.806199, tolerance = 1e-6)
+  expect_equal(gravity_station(0,0), 9.78032677, tolerance = 1e-6)
+  expect_equal(gravity_station(pi/2, 0), 9.832186, tolerance = 1e-6)
+  expect_equal(gravity_station(pi/4, 0), 9.806199, tolerance = 1e-6)
 
-  expect_equal(earthtide::gravity_station(0,1000), 9.777257, tolerance = 1e-6)
-  expect_equal(earthtide::gravity_station(0, 10000), 9.749696, tolerance = 1e-6)
+  expect_equal(gravity_station(0,1000), 9.777257, tolerance = 1e-6)
+  expect_equal(gravity_station(0, 10000), 9.749696, tolerance = 1e-6)
 
 })
