db1903a4a9f1d11ca0d491df7eca4a3e3969124b
jkennel <jkennel@uoguelph.ca>
2019-03-19 10:43:29 -0400

    remove onload test

--- tests/testthat/test-onload.R
+++
@@ -1,9 +0,0 @@
-context("zzz.R - .onLoad")
-
-test_that(".onLoad loads data", {
-  
-  obj_names <- c("hw95s", "dut1", "simon_coef_1994", 'ksm04')
-
-  expect_true(all(obj_names %in% data(package ='earthtide')$results[,3]))
-  
-})
