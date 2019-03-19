5307d7693de06821940d3f65736ba401a0620c0f
jkennel <jkennel@uoguelph.ca>
2019-03-19 10:35:16 -0400

    fix parameter

--- R/prepare_datetime.R
+++ R/prepare_datetime.R
@@ -3,8 +3,8 @@
   utc     <- sort(utc)
   mjd     <- utc_mod_julian(utc)
   j2000   <- utc_julian_2000(utc)
-  ddt     <- interpolate_dut1(self$datetime$utc, 'ddt') #delta_t(utc)
-  dut1    <- interpolate_dut1(self$datetime$utc, 'ut1_utc')  #ut1_utc(utc)
+  ddt     <- interpolate_dut1(utc, 'ddt') 
+  dut1    <- interpolate_dut1(utc, 'ut1_utc') 
   hours   <- (as.numeric(utc) %% 86400.0 + dut1) / 3600.0
   t_astro <- (j2000 + ddt / 3155760000.0) / 10.0
 
