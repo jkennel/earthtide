7a31bb76e4efb6edc7231e840296fa82e6fc6ed6
jkennel <jkennel@uoguelph.ca>
2019-06-17 11:59:02 -0400

    skip download test

--- tests/testthat/test-get_iers.R
+++ tests/testthat/test-get_iers.R
@@ -2,8 +2,10 @@ context("test-get_iers")
 
 test_that("get iers works", {
   
-  eop <- get_iers()
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
   
+  eop <- get_iers()
   expect_s3_class(eop, 'data.frame')
   expect_equal(ncol(eop), 8L)
   expect_gt(nrow(eop), 20990)
