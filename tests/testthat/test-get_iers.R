e5eafc04009d935649252a823ddbe4b797685a89
Jonathan Kennel <jkennel@uoguelph.ca>
2024-03-04 16:36:47 -0500

    don't run get_iers test under CI, CRAN and if offline

--- tests/testthat/test-get_iers.R
+++ tests/testthat/test-get_iers.R
@@ -2,7 +2,8 @@ context("test-get_iers")
 
 test_that("get iers works", {
   testthat::skip_on_cran()
-  testthat::skip_on_travis()
+  testthat::skip_on_ci()
+  testthat::skip_if_offline()
 
   eop <- get_iers()
   expect_s3_class(eop, "data.frame")
